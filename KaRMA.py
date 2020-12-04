87a2f746a18cc282c1b9b780bd1f7e6c907feab0
Ilan ATTIAS <attias.sin@gmail.com>
2020-12-04 21:54:19 +0100

    token update

--- KaRMA.py
+++ KaRMA.py
@@ -10,7 +10,7 @@ import asyncio
 from discord.ext import commands
 from CONST import constants
 
-import Weather as wth
+import Command as cmd
 
 
 TOKEN = constants.TOKEN
@@ -24,18 +24,18 @@ client = commands.Bot(command_prefix='kc!')
 #Weather Command
 @client.command()
 async def weather(ctx, *args):
-    await ctx.send(wth.Weather.getWeather('-'.join(args)))
+    await ctx.send(cmd.Weather.getWeather('-'.join(args)))
 
 #Cocktail Command
 @client.command()
 async def cocktail(ctx):
-    await ctx.send(embed = wth.Cocktail.getCocktail())
+    await ctx.send(embed = cmd.Cocktail.getCocktail())
 
 #Season Release Command
 @client.command()
-async def season(ctx, *args):
+async def upcoming(ctx, *args):
     reactions = ['⏮️','◀️', '▶️', '⏭️']
-    message = await ctx.send(embed = wth.Anime.getSeasonAnime())
+    message = await ctx.send(embed = cmd.Anime.getSeasonAnime())
     for reaction in reactions:
         await message.add_reaction(reaction)
     
@@ -43,33 +43,33 @@ async def season(ctx, *args):
 #Anime Command
 @client.command()
 async def anime(ctx, *args):
-    await ctx.send(wth.Anime.getAnime)
+    await ctx.send(cmd.Anime.getAnime)
 
 @client.command()
 async def random(ctx, *args):
     if args[0]=="apex":
-        await ctx.send(f"{ctx.author} you play {wth.Random.apexLoadout()}")
+        await ctx.send(f"{ctx.author} you play {cmd.Random.apexLoadout()}")
     elif args[0]=="ow":
         role = "all"
         if len(args)>1:
             role = args[1]
-        await ctx.send(wth.Random.owChar(role))
+        await ctx.send(cmd.Random.owChar(role))
     elif args[0]=="r6" and len(args)>1:
-        await ctx.send(wth.Random.r6Char(args[1]))
+        await ctx.send(cmd.Random.r6Char(args[1]))
     elif args[0]=="lol":
-        await ctx.send(wth.Random.lolChar())
+        await ctx.send(cmd.Random.lolChar())
     
 @client.command()
 async def sethome(ctx, *args):
     if len(args) < 3:
         return
     
-    wth.Minecraft.setHome(ctx.author, *args)
+    cmd.Minecraft.setHome(ctx.author, *args)
     await ctx.send("New Home Set")
 
 @client.command()
 async def home(ctx, arg = None):
-    await ctx.send(embed = wth.Minecraft.showHomes(arg))
+    await ctx.send(embed = cmd.Minecraft.showHomes(arg))
 
 
 @client.event
@@ -79,23 +79,23 @@ async def on_raw_reaction_add(payload):
 
     message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
     if payload.emoji.name == '⏮️':
-        await message.edit(embed=wth.Anime.getSeasonAnime(0))
+        await message.edit(embed=cmd.Anime.getSeasonAnime(0))
     elif payload.emoji.name == '◀️':
         animeId = int(message.embeds[0].footer.text.split("/")[0])-2
 
         if animeId == -1:
             animeId = 49
 
-        await message.edit(embed=wth.Anime.getSeasonAnime(animeId))
+        await message.edit(embed=cmd.Anime.getSeasonAnime(animeId))
     elif payload.emoji.name == '▶️':
         animeId = int(message.embeds[0].footer.text.split("/")[0])
 
         if animeId == 50:
             animeId = 0
 
-        await message.edit(embed=wth.Anime.getSeasonAnime(animeId))
+        await message.edit(embed=cmd.Anime.getSeasonAnime(animeId))
     elif payload.emoji.name == '⏭️':
-        await message.edit(embed=wth.Anime.getSeasonAnime(49))
+        await message.edit(embed=cmd.Anime.getSeasonAnime(49))
     
     await message.remove_reaction(payload.emoji.name, payload.member)
 
